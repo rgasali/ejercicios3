772dada9418b6ad4f3774f0ee14eb4f98b35019f
rgasali <96292535+rgasali@users.noreply.github.com>
2022-05-20 06:24:35 -0300

    con funciones

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,44 @@
+//declaracion de variables
 let divMostrarArreglos = <HTMLDivElement>(
   document.getElementById("divMostrarArreglos")
 );
-let arregloA: number[] = new Array(0, 1, 2, 3, 4, 5);
-let arregloB: number[] = new Array(0, 1, 2, 3, 4, 5);
+let btnCargaArregloA = <HTMLButtonElement>(
+  document.getElementById("btnCargaArregloA")
+);
+let btnCargaArregloB = <HTMLButtonElement>(
+  document.getElementById("btnCargaArregloB")
+);
+let arregloA: number[] = new Array(5);
+let arregloB: number[] = new Array(5);
 let concatenacionDeArreglos = "";
 
-for (let i = 0; i < arregloA.length; i++) {
-  concatenacionDeArreglos =
-    concatenacionDeArreglos + ` A${arregloA[i]} B${arregloB[i]} , `;
+//declaracion de funciones
+function generarArreglo(arreglo: Array<number>) {
+  for (let i = 0; i < arreglo.length; i++) {
+    arreglo[i] = Number(prompt("ingrese un valor al arreglo"));
+  }
 }
 
-divMostrarArreglos.innerHTML = String(
-  `los valores de los arrelos son: ${concatenacionDeArreglos}`
-);
+function concatenarDosArreglos(
+  arreglo1: Array<number>,
+  arreglo2: Array<number>
+) {
+  for (let i = 0; i < arreglo1.length; i++) {
+    concatenacionDeArreglos =
+      concatenacionDeArreglos + ` A${arreglo1[i]} B${arreglo2[i]} , `;
+  }
+}
+
+//resto del codigo
+
+btnCargaArregloA.addEventListener("click", () => {
+  generarArreglo(arregloA);
+});
+
+btnCargaArregloB.addEventListener("click", () => {
+  generarArreglo(arregloB);
+  concatenarDosArreglos(arregloA, arregloB);
+  divMostrarArreglos.innerHTML = String(
+    `los valores de los arrelos son: ${concatenacionDeArreglos}`
+  );
+});
